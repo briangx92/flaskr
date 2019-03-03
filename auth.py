ab661e70b389edcfc92ff8be8e5f2a02bab43021
Brian Gomez <briangx92@gmail.com>
2019-03-03 08:40:53 -0500

    updated

--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
 import functools
 
-from flask import(
+from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -15,15 +16,16 @@ def register():
         password = request.form['password']
         db = get_db()
         error = None
-        
+
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
         elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
-        
+
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
@@ -36,6 +38,7 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -44,14 +47,14 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)).fetchone()
-        
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
-        
+
         if error is None:
             session.clear()
             session['user_id'] = user['id']
@@ -60,6 +63,7 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -72,10 +76,12 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
 
 def login_required(view):
     @functools.wraps(view)
@@ -84,5 +90,5 @@ def login_required(view):
             return redirect(url_for('auth.login'))
 
         return view(**kwargs)
-    
+
     return wrapped_view
