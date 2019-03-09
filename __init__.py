34d01a67a398d7540fd03829435271c93ad8993c
Brian Gomez <briangx92@gmail.com>
2019-03-09 11:27:02 -0500

    fixed errors

--- __init__.py
+++ __init__.py
@@ -10,10 +10,6 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    return app
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -28,17 +24,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-        
     from . import db
     db.init_app(app)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    from . import auth
+    app.register_blueprint(auth.bp)
+
 
     return app
-
--- blog.py
+++ blog.py
@@ -44,33 +44,6 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 app.register_blueprint()
 
-@bp.route('/')
-def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
-
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
